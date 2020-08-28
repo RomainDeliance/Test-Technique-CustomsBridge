174b777db3c57fb84d303edcb139dab4dbc404e0
rdeliance <romain.deliance@epitech.eu>
2020-08-28 13:14:24 +0200

    [+] creating repository/readME.md + setup vueJS project

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Test-Technique-CustomsBridge
+# test
+
+## Get project
+```
+git clone https://github.com/RomainDeliance/Test-Technique-CustomsBridge.git
+cd Test-Technique-CustomsBridge
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### View Project
+```
+Go to http://localhost:8080/
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <HelloWorld></HelloWorld>
+    <v-row>
+      <v-col style="border: 1px" cols="12" lg="12" md="6" sm="3">
+        {{data}}
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      data: {},
+    }
+  },
+  beforeMount() {
+    this.getName();
+  },
+  methods: {
+    async getName() {
+      const res = await fetch('https://api.agify.io/?name=michael');
+      const data = await res.json();
+      this.data = data;
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
